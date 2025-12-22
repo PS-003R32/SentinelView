921fedf914b6ff4b6132b5a5229ef61247c9beb3
PS-003R32 <pramaniksuvadip10@gmail.com>
2025-12-22 10:44:18 +0530

    gateway update 2212

--- zeroW/gateway.py
+++ zeroW/gateway.py
@@ -26,4 +26,38 @@ def calculate_risk(threat, cpu):
 
     cpu_factor = 10 if cpu > 80 else 0
     return min(100, base_score + cpu_factor)
-  
+def main():
+   print("Starting Gateway...")
+   ser = serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=1)
+   current_threat = "None"
+
+   while True:
+      try:
+        if ser.in_waiting > 0:
+          line=ser.readline().decode('utf-8').strip()
+          try:
+            data = json.loads(line)
+            current_threat = data.get("threat", "None")
+            print(f"Update recieved: {current_threat}")
+          except json.JSONDecodeError:
+            pass
+        cpu=psutil.cpu_percent(interval=None)
+        ram = psutil.virtual_memory().percent
+        risk = calculate_risk(current_threat, cpu)
+
+        conn = get_db_connection()
+        if conn:
+          cur= conn.cursor()
+          cur.execute("""
+                      INSERT INTU telemetry (device_id, cpu_usage, ram_usage, threat_detected, risk_score) VALUES (%s,%s,%s,%s,%s)""", (DEVICE_ID, cpu,ram, current_threat, risk))
+          conn.commit()
+          cur.close()
+          conn.close()
+          print(f"Logged: {current_threat} | Risk: {risk}")
+        time.sleep(2)
+      except Exception as e:
+        print(f"Loop Error: {e}")
+        time.sleep(1)
+if __name__=='__main__':
+  main()
+         
