6bae2883030f62064fdee80b685e7542dae218a2
PS-003R32 <pramaniksuvadip10@gmail.com>
2025-12-26 10:07:57 +0530

    update struct

---
+++ gateway/gateway.py
@@ -0,0 +1,85 @@
+import serial
+import time
+import json
+import psutil
+import psycopg2
+from datetime import datetime
+import sys
+import serial.tools.list_ports
+import requests
+
+WEBHOOK_URL = "https://hook.eu1.make.com/" #you insert yoursd
+
+DB_HOST = "aws-x-xx-xxxxxxxx-1.xxxxx.supabase.com"#you insert yoursd
+DB_PORT = "xxxx"#you insert yoursd, usualy 6543
+DB_NAME = "xxxxxxx"#you insert yoursd
+DB_USER = "postgres.xxxxxxxxxxxx"#you insert yoursd
+DB_PASS = "passwd"#you insert yoursd 
+
+DEVICE_ID = "SENTINEL-ZERO-01"
+
+print(f"Sentinel Gateway Starting...")
+
+def find_pico():
+    ports = serial.tools.list_ports.comports()
+    for p in ports:
+        if "ACM" in p.device or "USB" in p.device: return p.device
+    return None
+
+port = find_pico()
+if not port:
+    print("Error: No Pico found. Check USB cable.")
+    sys.exit(1)
+
+print(f"Connecting to Pico at {port}...")
+
+try:
+    ser = serial.Serial(port, 115200, timeout=1)
+    ser.flush()
+except Exception as e:
+    print(f"Access Denied: {e}")
+    sys.exit(1)
+
+print("System Active. Waiting for attacks...")
+
+while True:
+    if ser.in_waiting:
+        try:
+            line = ser.readline().decode('utf-8').strip()
+            if not line.startswith("{"): continue
+            
+            data = json.loads(line)
+            threat = data.get("threat", "Unknown")
+            risk = data.get("risk", 0)
+            
+            print(f"\nINTRUSION DETECTED: {threat} (Risk: {risk})")
+            
+            print("   -> Sending to Cloud...", end=" ")
+            try:
+                params = {'device': DEVICE_ID, 'threat': threat, 'risk': risk}
+                requests.get(WEBHOOK_URL, params=params, timeout=5)
+                print("OK")
+            except Exception as e:
+                print(f"FAILED ({e})")
+
+            print("   -> Logging to Database...", end=" ")
+            try:
+                conn = psycopg2.connect(
+                    host=DB_HOST, database=DB_NAME, user=DB_USER, 
+                    password=DB_PASS, port=DB_PORT, sslmode='require'
+                )
+                cur = conn.cursor()
+                cur.execute(
+                    "INSERT INTO telemetry (device_id, threat_detected, risk_score) VALUES (%s, %s, %s)", 
+                    (DEVICE_ID, threat, risk)
+                )
+                conn.commit()
+                conn.close()
+                print("OK")
+            except Exception as e:
+                print(f"FAILED ({e})")
+
+        except Exception as e:
+            print(f"Error: {e}")
+            
+    time.sleep(0.01)
